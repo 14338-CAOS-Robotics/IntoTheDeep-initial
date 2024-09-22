f612d4e9e5ed78805d8415454df39f2c30f3f5f1
csolomonson <ctsolomonson@gmail.com>
2024-09-21 17:00:10 -0700

    Make Outtake skeleton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Outtake.java
@@ -1,95 +1,66 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
-
-import static org.firstinspires.ftc.teamcode.Hardware.Robot.OuttakeConstants.armExtend;
-import static org.firstinspires.ftc.teamcode.Hardware.Robot.OuttakeConstants.armZero;
-import static org.firstinspires.ftc.teamcode.Hardware.Robot.OuttakeConstants.lOCClose;
-import static org.firstinspires.ftc.teamcode.Hardware.Robot.OuttakeConstants.lOCOpen;
-import static org.firstinspires.ftc.teamcode.Hardware.Robot.OuttakeConstants.rOCClose;
-import static org.firstinspires.ftc.teamcode.Hardware.Robot.OuttakeConstants.rOCOpen;
-import static org.firstinspires.ftc.teamcode.Hardware.Robot.OuttakeConstants.wristExtend;
-import static org.firstinspires.ftc.teamcode.Hardware.Robot.OuttakeConstants.wristZero;
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.hardwareMap;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.Hardware.Physical.CAOSHardware;
+
+/**
+ * This class just handles releasing the Pez.
+ * To move the outtake arm, use Transfer class
+ */
 public class Outtake {
+    Servo pitch, roll, effect;
+    DcMotorEx lift;
+    public Outtake(CAOSHardware hardware) {
+        pitch = hardwareMap.servo.get("op");
+        roll = hardwareMap.servo.get("or");
+        effect = hardwareMap.servo.get("oe");
 
-    public boolean v4bZero = false;
-    boolean wristZero = true;
-    boolean clawsZero = false;
-    Servo l4b, r4b, armJoint, rightOuttakeClaw, leftOuttakeClaw;
-    public Outtake() {
-        l4b = hardwareMap.servo.get("l4b");
-        r4b = hardwareMap.servo.get("r4b");
-        armJoint = hardwareMap.servo.get("aj");
-        rightOuttakeClaw = hardwareMap.servo.get("roc");
-        leftOuttakeClaw = hardwareMap.servo.get("loc");
-
-        r4b.setDirection(Servo.Direction.REVERSE);
-        rightOuttakeClaw.setDirection(Servo.Direction.REVERSE);
-
+        lift = (DcMotorEx) hardware.lift;
     }
 
-    public void cycleV4b() {
-        if (!v4bZero) {
-            l4b.setPosition(armZero);
-            r4b.setPosition(armZero);
-            v4bZero = true;
-        } else {
-            l4b.setPosition(armExtend);
-            r4b.setPosition(armExtend);
-            v4bZero = false;
-        }
+    /**
+     * Lift/lower the outtake apparatus
+     * @param power Positive for up, negative for down (-1 to 1)
+     */
+    public void doTheLifts(double power) {
+
     }
 
-    public void cycleWrist() {
-        if(wristZero) {
-            armJoint.setPosition(wristExtend);
-            wristZero = false;
-        } else {
-            armJoint.setPosition(Robot.OuttakeConstants.wristZero);
-            wristZero = true;
-        }
-    }
-
-    public void cycleOuttakeClaw() {
-        if(clawsZero) {
-            leftOuttakeClaw.setPosition(lOCOpen);
-            rightOuttakeClaw.setPosition(rOCOpen);
-            clawsZero = false;
-        } else {
-            leftOuttakeClaw.setPosition(lOCClose);
-            rightOuttakeClaw.setPosition(rOCClose);
-            clawsZero = true;
-        }
-    }
-    public void transferPosition() {
-        l4b.setPosition(armZero);
-        r4b.setPosition(armZero);
-        armJoint.setPosition(Robot.OuttakeConstants.wristZero);
-        closeClaw();
-    }
-    public void depositPosition() {
-        l4b.setPosition(armExtend);
-        r4b.setPosition(armExtend);
-        armJoint.setPosition(wristExtend);
-        openClaw();
-    }
-    public void transferNoClawPos() {
-        l4b.setPosition(armZero);
-        r4b.setPosition(armZero);
-        armJoint.setPosition(Robot.OuttakeConstants.wristZero);
-    }
+    /**
+     * Close the claw to grab a Pez
+     */
     public void closeClaw() {
-        leftOuttakeClaw.setPosition(lOCClose);
-        rightOuttakeClaw.setPosition(rOCClose);
-        clawsZero = true;
+
     }
 
+    /**
+     * Drop a Pez
+     */
     public void openClaw() {
-        leftOuttakeClaw.setPosition(lOCOpen);
-        rightOuttakeClaw.setPosition(rOCOpen);
-        clawsZero = false;
+
     }
+
+    /**
+     * Move the outtake like a hinge
+     * @param pitchServoValue Servo something idk
+     */
+    public void setPitch(double pitchServoValue) {
+
+    }
+
+    /**
+     *
+     * Move the outtake like a wrist
+     * @param rollServoValue We'll figure that out later (ticks?)
+     */
+    public void setRoll(double rollServoValue) {
+
+    }
+
+
+
 }
