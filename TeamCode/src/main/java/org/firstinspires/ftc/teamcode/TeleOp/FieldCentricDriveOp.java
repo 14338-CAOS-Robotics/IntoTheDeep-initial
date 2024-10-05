ddc79a5e0ceea83b493cd20c5697197e40d2d5a9
Cole Solomonson <72103122+csolomonson@users.noreply.github.com>
2024-10-05 13:54:21 -0700

    Add telemetry, document, and refactor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricDriveOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricDriveOp.java
@@ -1,11 +1,18 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.GamepadControls.Controller;
 import org.firstinspires.ftc.teamcode.GamepadControls.JoystickControls;
 import org.firstinspires.ftc.teamcode.Hardware.Robot;
+import org.firstinspires.ftc.teamcode.Utilities.OpModeUtils;
 
+/**
+ * Controller 1 controls in field centric mode. Controller 2 in robot centric mode
+ */
+@TeleOp
 public class FieldCentricDriveOp extends LinearOpMode {
 
     Controller gp1;
@@ -13,28 +20,34 @@ public class FieldCentricDriveOp extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        OpModeUtils.setOpMode(this);
         gp1 = new Controller(gamepad1);
         gp2 = new Controller(gamepad2);
         Robot robot = new Robot();
         robot.setController1(gp1);
         robot.setController2(gp2);
         waitForStart();
+
         while (opModeIsActive()) {
             robot.update();
 
-            robot.setFieldDrivePower(
-                    gp1.get(JoystickControls.Input.LEFT, JoystickControls.Value.INVERT_X),
-                    gp1.get(JoystickControls.Input.LEFT, JoystickControls.Value.Y),
-                    gp1.get(JoystickControls.Input.RIGHT, JoystickControls.Value.X)
-            );
-            if (gp1.get(JoystickControls.Input.LEFT, JoystickControls.Value.INVERT_X) == 0 &&
-            gp1.get(JoystickControls.Input.LEFT, JoystickControls.Value.Y) == 0 && gp1.get(JoystickControls.Input.RIGHT, JoystickControls.Value.X) == 0) {
+            if (gp1.joyStickUsed(JoystickControls.Input.LEFT) || gp1.joyStickUsed(JoystickControls.Input.RIGHT)) {
+                robot.setFieldDrivePower(
+                        gp1.get(JoystickControls.Input.LEFT, JoystickControls.Value.INVERT_X),
+                        gp1.get(JoystickControls.Input.LEFT, JoystickControls.Value.Y),
+                        gp1.get(JoystickControls.Input.RIGHT, JoystickControls.Value.X)
+                );
+            } else {
                 robot.setRobotDrivePower(
-                        gp2.get(JoystickControls.Input.LEFT, JoystickControls.Value.INVERT_X),
                         gp2.get(JoystickControls.Input.LEFT, JoystickControls.Value.Y),
+                        gp2.get(JoystickControls.Input.LEFT, JoystickControls.Value.INVERT_X),
                         gp2.get(JoystickControls.Input.RIGHT, JoystickControls.Value.X)
                 );
             }
+            telemetry.addData("lefty", gp1.get(JoystickControls.Input.LEFT, JoystickControls.Value.Y));
+            telemetry.addData("leftx", gp1.get(JoystickControls.Input.LEFT, JoystickControls.Value.X));
+            telemetry.addData("rightx", gp1.get(JoystickControls.Input.RIGHT, JoystickControls.Value.X));
+            telemetry.update();
         }
     }
 }
